71e8fd5c6b1ec2e8e68473e28e6b715552592278
ask-athu <askatharvaa@gmail.com>
2026-07-12 11:17:12 +0530

    Updated Type Casting

---
+++ 01-Python-Fundamentals/AdditionTypeCastX.py
@@ -0,0 +1,9 @@
+print ("Enter Number 1 : ")
+iNo1 = int (input())
+
+print ("Enter Number 2 : ")
+iNo2 = int (input())
+
+iResult = iNo1 + iNo2        
+
+print (f"Addition of {iNo1} and {iNo2} : {iResult}")
