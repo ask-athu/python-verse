9ccefd641183b5935017bfb260cbb3a5fb5a24b2
ask-athu <askatharvaa@gmail.com>
2026-07-12 11:14:05 +0530

    Data types - Numeric and Boolean
    Add a demo script for various Python data types including numeric, boolean, and NoneType.

---
+++ 01-Python-Fundamentals/typeDemo.py
@@ -0,0 +1,18 @@
+# NUMERIC DATA TYPE 
+no = 11         # int
+marks = 73.3    # float
+value = 8+5j    # complex
+
+print (type(no))
+print (type(marks))
+print (type(value))
+
+#bool  (BOOLEAN)
+gender = True
+print (gender)
+print (type(gender))
+
+#NoneType (Just creating a variable, with value as None (Null) )
+demo = None
+print (demo)
+print (type(demo))
