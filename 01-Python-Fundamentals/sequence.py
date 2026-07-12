47e13cfc2f1c7b8e6740787651d3ae1bce09845c
ask-athu <askatharvaa@gmail.com>
2026-07-12 11:19:15 +0530

    Examples of Iterable data types

---
+++ 01-Python-Fundamentals/sequence.py
@@ -0,0 +1,24 @@
+data1 = [10,20,30]      #list : (Works like Array)
+
+data2 = (10,20,30)      #tuple
+
+data3 = {10,20,30}      #tet
+
+data4 = {"A" : 10, "B" : 20, "C" : 30}      #dict (Mapping) but consider as a sequence
+
+data5 = "MUMBAI"        #str
+
+print(data1)
+print(type(data1))
+
+print(data2)
+print(type(data2))
+
+print(data3)
+print(type(data3))
+
+print(data4)
+print(type(data4))
+
+print(data5)
+print(type(data5))
