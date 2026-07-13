a60ddfa94efd289e20d3dae4ece8a601f88d0092
Atharva Vichare <askatharvaa@gmail.com>
2026-07-13 23:06:14 +0530

    Use of range ()
    Demonstrate the usage of range in Python with examples.

---
+++ 01-Python-Fundamentals/rangeDemo.py
@@ -0,0 +1,12 @@
+iData1 = range(5) 
+
+print (iData1)
+print (type(iData1))
+
+iData2 = range(1,10,2) 
+
+print (iData2)
+print (type(iData2))
+print (len(iData2))         # Output : 5 (ie- 1 3 5 7 9)
+
+# PROVES that range is data type, as len() only works on sequence data type.
