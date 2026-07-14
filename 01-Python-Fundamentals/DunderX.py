a42f6f0de99237ec265f70ad03b6363187e93da6
ask-athu <askatharvaa@gmail.com>
2026-07-14 23:13:05 +0530

    Main () using __name__

---
+++ 01-Python-Fundamentals/DunderX.py
@@ -0,0 +1,8 @@
+# print ( __name__ )   This is Dunder variable (starts and ends w 2 underscore)
+
+def main () :
+    print ("Inside main () ")
+
+if ( __name__ == "__main__" ) :
+    main ()                         # We call user-def main() 
+
