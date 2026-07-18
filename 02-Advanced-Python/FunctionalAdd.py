2fe557702da8b982417ce70314bcde9029bd3cfc
ask-athu <askatharvaa@gmail.com>
2026-07-18 22:59:48 +0530

    Functional - Add ()

---
+++ 02-Advanced-Python/FunctionalAdd.py
@@ -0,0 +1,5 @@
+Add = lambda iNo1, iNo2 : iNo1 + iNo2
+
+iResult = Add (10,11)       # Execution starts here ( Compiler would make : iResult = 10 + 11 )
+
+print ("Addition is : ", iResult) # IMP : functions are treated as first-class objects
