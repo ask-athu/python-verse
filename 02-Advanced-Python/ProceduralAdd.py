9afc47994e88e6be7c5ced345c289aa770bf573b
ask-athu <askatharvaa@gmail.com>
2026-07-18 22:58:50 +0530

    Procedural - Add ()

---
+++ 02-Advanced-Python/ProceduralAdd.py
@@ -0,0 +1,11 @@
+def Add (iNo1, iNo2) :
+    return (iNo1 + iNo2)
+
+
+def main () :
+    iResult = Add (10, 11) 
+    print ("Addition is : ", iResult)
+
+
+if (__name__ == "__main__") :
+    main ()
