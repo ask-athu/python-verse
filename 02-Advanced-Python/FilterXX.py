3f4695e14f503c36eca9b389d58d9c863a9d764a
ask-athu <askatharvaa@gmail.com>
2026-07-20 19:55:28 +0530

    Filter Function with lambda - 02

--- 02-Advanced-Python/FilterXX.py
+++ 02-Advanced-Python/FilterXX.py
@@ -1,4 +1,3 @@
-# FilterX -> bcuz there exists an inbuild module with name Filter.py
 # Input dataset -> List -> Filters (Removes Odd Numbers) -> Stores Result Set in Fdata (new list) 
 
 checkEven  = lambda iNo : (iNo % 2 == 0)
@@ -14,6 +13,3 @@ def main () :
 
 if (__name__ == "__main__") :
     main ()
-
-
-# NOTE : 1st Parameter of filter function (here checkEven) MUST ALWAYS return bool and that function must accept ONLY 1 PARAMETER
