285eda8643e7ce9c817cff57b49086f83360ffed
ask-athu <askatharvaa@gmail.com>
2026-07-20 20:04:24 +0530

    Map Function with lambda - 02

---
+++ 02-Advanced-Python/MapXX.py
@@ -0,0 +1,23 @@
+# MapX -> bcuz there might exist an inbuild module with name Filter.py
+# Input dataset[] -> List -> Filters (Removes Odd Numbers) -> Stores Result Set in Fdata[] (new list) -> Map FData (Add 1) -> Stores in Mdata[]
+
+checkEven = lambda iNo : (iNo % 2 == 0)
+
+Increment = lambda iNo : (iNo + 1)
+
+def main () :
+    Data = [13,12,8,10,11,20]
+    
+    print ("Present Dataset : ",Data)
+
+    FData = list ( filter (checkEven, Data) )
+    print ("Filterd Dataset : ",FData)
+
+    Mdata = list ( map (Increment, FData) )
+    print ("Mapped Dataset : ",Mdata)
+
+if (__name__ == "__main__") :
+    main ()
+
+
+# NOTE : 1st Parameter of filter function (here checkEven) MUST ALWAYS return bool
