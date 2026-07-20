b63305ca0b2af1941eca12424befa6a886d03030
ask-athu <askatharvaa@gmail.com>
2026-07-20 19:42:10 +0530

    Filter Function - 01

---
+++ 02-Advanced-Python/FilterX.py
@@ -0,0 +1,20 @@
+# FilterX -> bcuz there exists an inbuild module with name Filter.py
+# Input dataset -> List -> Filters (Removes Odd Numbers) -> Stores Result Set in Fdata (new list) 
+
+def checkEven (iNo) :
+    return (iNo % 2 == 0)
+
+def main () :
+    Data = [13,12,8,10,11,20]
+    
+    print ("Present Dataset : ",Data)
+
+    FData = list ( filter (checkEven, Data) )
+
+    print ("Filterd Dataset : ",FData)
+
+if (__name__ == "__main__") :
+    main ()
+
+
+# NOTE : 1st Parameter of filter function (here checkEven) MUST ALWAYS return bool
