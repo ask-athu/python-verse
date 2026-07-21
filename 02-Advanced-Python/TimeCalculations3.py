fcd1cc6c602deab50bb3ce192cd8678b2d1563f5
ask-athu <askatharvaa@gmail.com>
2026-07-22 00:17:22 +0530

    Time Module - 01X

---
+++ 02-Advanced-Python/TimeCalculations3.py
@@ -0,0 +1,26 @@
+import time 
+
+def main () :
+    iValue = int (input ("Enter number to check factorial : "))
+
+    start_time = time.time() 
+
+    iResult = Factorial (iValue)
+    print (f"Factorial of {iValue} is : {iResult}")
+
+    end_time = time.time()
+
+    print ("Start Time : ", start_time)
+    print ("End Time : ", end_time)
+    print (f"Time taken to complete logic : {end_time - start_time:.5f} seconds") 
+
+def Factorial (iNo) :
+    iFact = 1
+
+    for i in range (1, (iNo+1) ) :
+        iFact = iFact * i 
+
+    return (iFact)
+
+if ( __name__ == "__main__" ) :
+    main ()
